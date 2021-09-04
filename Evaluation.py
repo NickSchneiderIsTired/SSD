917175d3b81d78360e24bdb8844aea8cc0719d5d
Nick Schneider <nick-s2000@web.de>
2021-09-04 21:57:15 +0200

    Exchange nms with tensorflow nms

--- Evaluation.py
+++ Evaluation.py
@@ -1,6 +1,3 @@
-import numpy as np
-from AnchorUtils import norm_img
-from AnnotationRect import iou, AnnotationRect
 import tensorflow as tf
 import os
 from os import listdir
@@ -11,46 +8,18 @@ def call_eval_script():
     os.system('python eval_detections.py --detection detections.txt --dset_basedir ./dataset_mmp/val/')
 
 
-def write_detections(filenames, boxes, scores):
+def write_detections(filename, boxes, scores):
     with open('detections.txt', 'a') as file:
-        for filename in filenames:
-            for (box, score) in zip(boxes, scores):
-                fn = filename.numpy().decode('utf-8')[-12:]
-                file.write(fn + ' 0 '
-                           + ' ' + str(box[0]) + ' ' + str(box[1]) + ' ' + str(box[2]) + ' ' + str(box[3])
-                           + ' ' + str(score) + '\n')
+        for (box, score) in zip(boxes, scores):
+            fn = filename.numpy().decode('utf-8')[-12:]
+            file.write(fn + ' 0 '
+                       + ' ' + str(box[0]) + ' ' + str(box[1]) + ' ' + str(box[2]) + ' ' + str(box[3])
+                       + ' ' + str(score) + '\n')
         file.close()
 
 
 def nms(boxes, scores, threshhold):
-    rows, cols, factor, scale, rect = np.shape(boxes)
-    boxes = np.reshape(boxes, (rows * cols * factor * scale, rect))
-    scores = np.reshape(scores.numpy(), (rows * cols * factor * scale))
-    D = np.empty((0, 4), dtype=np.int)
-    S = np.empty(0)
-    length = len(boxes)
-    while length > 0:
-        m = np.argmax(scores)
-        if scores[m] == 0:
-            break
-        M = boxes[m]
-
-        D = np.append(D, [M], axis=0)
-        boxes = np.delete(boxes, m, axis=0)
-
-        S = np.append(S, scores[m])
-        scores = np.delete(scores, m, axis=0)
-        length -= 1
-        i = 0
-        while i < length:
-            ratio = iou(AnnotationRect.fromarray(M), AnnotationRect.fromarray(boxes[i]))
-            if ratio >= threshhold:
-                boxes = np.delete(boxes, i, axis=0)
-                length -= 1
-                scores = np.delete(scores, i, axis=0)
-            else:
-                i += 1
-    return D, S
+    return tf.image.non_max_suppression(boxes, scores, iou_threshold=threshhold, max_output_size=20)
 
 
 def evaluate_net(net, grid, dataset_path):
@@ -62,6 +31,8 @@ def evaluate_net(net, grid, dataset_path):
                           num_parallel_calls=2,
                           anchor_grid=grid,
                           threshhold=0.9)
+    flatted_grid = tf.reshape(grid, (10 * 10 * 4 * 3, 4))
+
     print("Starting evaluation")
     for (filenames, imgs) in dataset():
         net_output = net(imgs, training=False)
@@ -69,9 +40,16 @@ def evaluate_net(net, grid, dataset_path):
 
         softmax_between_person_and_background = tf.nn.softmax(net_output)
         person_scores = softmax_between_person_and_background[:, :, :, :, :, 1]
-        box_score_tuples = [nms(grid, max, 0.3) for max in person_scores]
-        for (bestBoxes, bestScores) in box_score_tuples:
-            write_detections(filenames, bestBoxes, bestScores)
+
+        for (filename, person_score) in zip(filenames, person_scores):
+            flatted_scores = tf.reshape(person_score, 10 * 10 * 4 * 3)
+            selected_indices = nms(tf.cast(flatted_grid, dtype=tf.float32), flatted_scores, 0.3)
+
+            selected_boxes = tf.gather(flatted_grid, selected_indices)
+            selected_scores = tf.gather(flatted_scores, selected_indices)
+
+            write_detections(filename, selected_boxes.numpy(), selected_scores.numpy())
+
     print("Evaluation completed")
 
 
